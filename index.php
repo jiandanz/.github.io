6d1c6270e401591d3f8e08805c5d17bb3f372c9c
jiandanz <45055910+jiandanz@users.noreply.github.com>
2018-11-16 08:43:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=400,target-densitydpi=high-dpi,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+<title>万讯视频网站在线解析VIP接口</title>
+<!--浏览器标签ico-->
+<link rel="shortcut icon" href="favicon.png" />
+<!--书签标签-->
+<link rel="bookmark" href="favicon.png" />
+<style type="text/css">
+<!--
+.foot-a {color: #0099FF}
+.foot-b {font-size: medium}
+.foot-c {color: #009900; font-weight: bold;}
+-->
+</style>
+</head>
+<body>
+<script type="text/javascript">
+<!--
+function wxkjsearch()
+{
+if(sousuo.url01.checked)
+window.open("http://api.bbbbbb.me/jx/?url="+sousuo.key.value,"");
+if(sousuo.url02.checked)
+window.open("http://jx.ejiafarm.com/dy.php?url="+sousuo.key.value,"");
+if(sousuo.url03.checked)
+window.open("https://jx.618g.com/?url="+sousuo.key.value,"");
+if(sousuo.url04.checked)
+window.open("http://api.wlzhan.com/sudu/?url="+sousuo.key.value,"");
+if(sousuo.url05.checked)
+window.open("http://jx.ejiafarm.com/x/jiexi.php?url="+sousuo.key.value,"");
+if(sousuo.url06.checked)
+window.open("http://www.wq114.org/tong.php?url="+sousuo.key.value,"");
+if(sousuo.url07.checked)
+window.open("http://api.nepian.com/ckparse/?url="+sousuo.key.value,"");
+if(sousuo.url08.checked)
+window.open("http://api.91exp.com/svip/?url="+sousuo.key.value,"");
+if(sousuo.url09.checked)
+window.open("http://jiexi.071811.cc/jx.php?url="+sousuo.key.value,"");
+if(sousuo.url10.checked)
+window.open("http://yun.baiyug.cn/vip/?url="+sousuo.key.value,"");
+if(sousuo.url11.checked)
+window.open("http://vip.jlsprh.com/index.php?url="+sousuo.key.value,"");
+if(sousuo.url12.checked)
+window.open("http://jqaaa.com/jx.php?url="+sousuo.key.value,"");
+if(sousuo.url13.checked)
+window.open("https://www.1717yun.com/jx/ty.php?url="+sousuo.key.value,"");
+if(sousuo.url14.checked)
+window.open("http://000o.cc/jx/ty.php?url="+sousuo.key.value,"");
+if(sousuo.url15.checked)
+window.open("http://aikan-tv.com/?url="+sousuo.key.value,"");
+if(sousuo.url16.checked)
+window.open("http://jiexi.92fz.cn/player/vip.php?url="+sousuo.key.value,"");
+return false;
+}
+--> 
+</script>
+
+<form method="post" name="sousuo" id="sousuo" style=" margin:0px; padding:0px" onsubmit="return(wxkjsearch())">
+  <p><a href="/" target="_blank"><img src="logo.png" alt="万讯VIP视频解析网" width="194" height="50" border="0" longdesc="http://www.wanxun.vip/jx" /></a><a href="/" title="万讯VIP视频解析网" target="_blank"></a><a href="/" target="_blank"></a></p>
+  <p><b><strong>视频网站在线解析接口</strong>:</b><br />
+http://www.wanxun.vip/jx/?url=
+<br />
+<input name="key" type="text" id="key" size="50" maxlength="200" />
+<br />
+<input type="submit" value="&nbsp;播放&nbsp;" />
+<br />
+<input type="radio" name="ss" id="url01" checked="checked" />
+①☞无名小站<br />
+<input type="radio" name="ss" id="url02" />
+②号通用vip引擎系统【万能接口】<br />
+<input type="radio" name="ss" id="url03" />
+③号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url04" />
+④号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url05" />
+⑤号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url06" />
+⑥号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url07" />
+⑦号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url08" />
+⑧号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url09" />
+⑨号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url10" />
+⑩号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url11" />
+⑪号vip引擎系统☜<br />
+<input type="radio" name="ss" id="url12" />
+⑫号vip引擎系统【金桥解析】<br />
+<input type="radio" name="ss" id="url13" />
+⑬号vip引擎系统【云视频解析】<br />
+<input type="radio" name="ss" id="url14" />
+⑭号vip引擎系统【腾讯视频解口】<br />
+<input type="radio" name="ss" id="url15" />
+⑮号vip引擎系统【爱看TV】<br />
+<input type="radio" name="ss" id="url16" />
+⑯号vip引擎系统【云解析】<br />
+  </p>
+</form>
+<p class="foot-b"><span class="foot-c">Copyright © 2018 <a href="http://www.wanxunkeji.cn" target="_blank">wanxun.vip</a></span></p>
+<script type="text/javascript" src="//js.users.51.la/19742001.js"></script>
+</body>
+</html>
